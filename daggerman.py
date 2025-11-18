ccea43ca4a5ad1471e90c7f5e45aa1d839b192c7
noah <143404345+NoahBrezet@users.noreply.github.com>
2025-11-18 19:18:57 +0000

    level up attacking monster and equip bug

--- daggerman.py
+++ daggerman.py
@@ -4,6 +4,7 @@ x = []
 y = []
 id = []
 standon = []
+
 exp = 0
 lvl = 1
 max_hp = 10
@@ -15,7 +16,17 @@ armor = "Nothing"
 armor_defense = 0
 extra_defense = 0
 extra_slot = "Nothing"
+
 board_dict = {}
+
+Mx = []
+My = []
+Mhp = []
+Mdamage = []
+Mdefense = []
+Mexp = []
+Mid = []
+MplaceID = []
 
 def roll(num_sides):
     roll = random.randint(1, num_sides)
@@ -87,26 +98,90 @@ def gen():
     elif lvl == 1:
         if n <= 25:
             place_object("S")
+            Mx.append(x[-1])
+            My.append(y[-1])
+            Mhp.append(2)
+            Mdamage.append(3)
+            Mdefense.append(0)
+            Mexp.append(1)
+            Mid.append("S")
+            MplaceID.append(len(id)-1)
         elif n <= 28:
             place_object("K")
+            Mx.append(x[-1])
+            My.append(y[-1])
+            Mhp.append(4)
+            Mdamage.append(2)
+            Mdefense.append(0)
+            Mexp.append(2)
+            Mid.append("K")
+            MplaceID.append(len(id)-1)
         else:
             place_object("C")
     elif lvl == 2:
         if n <= 24:
             place_object("K")
+            Mx.append(x[-1])
+            My.append(y[-1])
+            Mhp.append(4)
+            Mdamage.append(2)
+            Mdefense.append(0)
+            Mexp.append(2)
+            Mid.append("K")
+            MplaceID.append(len(id)-1)
         elif n <= 26:
             place_object("L")
+            Mx.append(x[-1])
+            My.append(y[-1])
+            Mhp.append(10)
+            Mdamage.append(4)
+            Mdefense.append(1)
+            Mexp.append(4)
+            Mid.append("L")
+            MplaceID.append(len(id)-1)
         elif n <= 28:
             place_object("F")
+            Mx.append(x[-1])
+            My.append(y[-1])
+            Mhp.append(6)
+            Mdamage.append(6)
+            Mdefense.append(0)
+            Mexp.append(3)
+            Mid.append("F")
+            MplaceID.append(len(id)-1)
         else:
             place_object("C")
     elif lvl >= 3:
         if n <= 24:
             place_object("F")
+            Mx.append(x[-1])
+            My.append(y[-1])
+            Mhp.append(6)
+            Mdamage.append(6)
+            Mdefense.append(0)
+            Mexp.append(3)
+            Mid.append("F")
+            MplaceID.append(len(id)-1)
         elif n <= 26:
             place_object("O")
+            Mx.append(x[-1])
+            My.append(y[-1])
+            Mhp.append(10)
+            Mdamage.append(8)
+            Mdefense.append(2)
+            Mexp.append(6)
+            Mid.append("O")
+            MplaceID.append(len(id)-1)
         elif n <= 28:
             place_object("T")
+            Mx.append(x[-1])
+            My.append(y[-1])
+            Mhp.append(16)
+            Mdamage.append(6)
+            Mdefense.append(3)
+            Mexp.append(7)
+            Mid.append("T")
+            MplaceID.append(len(id)-1)
         else:
             place_object("C")
 
@@ -115,8 +190,8 @@ def equip():
     choice = input()
     return choice
 
-
 def attack(dx, dy):
+    global action_taken, weapon, weapon_damage, armor, armor_defense, extra_slot, extra_damage, extra_defense, exp, lvl, max_hp, hp
     k = id.index("x")
     target_x = x[k] + dx
     target_y = y[k] + dy
@@ -163,7 +238,7 @@ def attack(dx, dy):
                             armor = "Chainmail Armor"
                             armor_defense = 2
                     elif treasure_roll == 3:
-                        print("You found a swift dagger! (Damage 1d3)")
+                        print("You found a swift dagger! (Damage 1d3, use q for swift attack)")
                         if equip() == "y":
                             weapon = "swift dagger"
                             weapon_damage = 3
@@ -198,7 +273,7 @@ def attack(dx, dy):
                             armor = "Plate Armor"
                             armor_defense = 3
                     elif treasure_roll == 3:
-                        print("You found a swift dagger! (Damage 1d4)")
+                        print("You found a swift dagger! (Damage 1d4, use q for swift attack)")
                         if equip() == "y":
                             weapon = "swift dagger"
                             weapon_damage = 4
@@ -223,7 +298,30 @@ def attack(dx, dy):
             elif id[n] != "□":
                 action_taken = True
                 print(f"You attack the {id[n]}!")
-                # Here you would implement combat mechanics
+                monster_index = MplaceID.index(n)
+                attack_roll = roll(weapon_damage) + extra_damage
+                print(f"You deal {attack_roll} damage!")
+                Mhp[monster_index] -= attack_roll
+                if Mhp[monster_index] <= 0:
+                    print(f"You defeated the {id[n]}!")
+                    exp_gain = Mexp[monster_index]
+                    print(f"You gain {exp_gain} EXP!")
+                    exp += exp_gain
+                    id[n] = "□"
+                    Mx.pop(monster_index)
+                    My.pop(monster_index)
+                    Mhp.pop(monster_index)
+                    Mdamage.pop(monster_index)
+                    Mdefense.pop(monster_index)
+                    Mexp.pop(monster_index)
+                    Mid.pop(monster_index)
+                    MplaceID.pop(monster_index)
+                    if exp >= lvl * 10:
+                        lvl += 1
+                        print(f"You leveled up! You are now level {lvl}!")
+                        max_hp += 5
+                        hp = max_hp
+                        print(f"Your max HP increased to {max_hp}!")
             else:
                 return
 
@@ -270,14 +368,16 @@ while True:
     elif action == "s":
         move(0, 1)
     elif action == "h":
-        print ("You are 'x'.")
-        print ("Squares you can stand on are marked with '□'.")
-        print ("After we ask you for your action you can type w a s or d to move.")
-        print ("Or you can type ww aa ss or dd to attack in that direction.")
-        print ("you can also type i to see your inventory and statts.")
-        print ("You can type m to see every monster that you can currently fight.")
-        print ("You can type quit to end the game.")
-        print ("if you want the instructions again type h.")
+        print ("w a s or d to move.")
+        print ("aa ss dd or ww to attack in that direction.")
+        print ("i - see your inventory and statts.")
+        print ("m - see monsters you can fight.")
+        if extra_slot == "health potion":
+            print ("r - restore hp to max.")
+        if weapon == "swift dagger":
+            print ("q - swift attack.")
+        print ("quirt - end the game.")
+        print ("h - see this help message again.")
         print ("Later you may get more options.")
         print ("You can open chests by attacking them.")
         print ("Good luck!")
@@ -294,15 +394,23 @@ while True:
                 print("O - Orc (HP: 10, Damage: 1d8, defense: 2)")
                 print("T - Troll (HP: 16, Damage: 1d6, defense: 3)")
     elif action == "aa":
-        action_taken = True
         attack(-1, 0)
     elif action == "dd":
-        action_taken = True
         attack(1, 0)
     elif action == "ww":
-        action_taken = True
         attack(0, -1)
     elif action == "ss":
-        action_taken = True
         attack(0, 1)
+    elif action == "r":
+        if extra_slot == "health potion":
+            hp = max_hp
+            print("You used the health potion and restored your HP to max!")
+            extra_slot = "Nothing"
+            action_taken = True
+    elif action == "q":
+        if weapon == "swift dagger":
+            attack(0, -1)
+            attack(-1, 0)
+            attack(1, 0)
+            attack(0, 1)
 # print ("GAME OVER")
