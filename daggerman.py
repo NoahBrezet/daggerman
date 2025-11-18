018221f77f55f67f8bc5e11b23d4b6e9d3d754d6
noah <143404345+NoahBrezet@users.noreply.github.com>
2025-11-18 17:14:50 +0000

    chests

--- daggerman.py
+++ daggerman.py
@@ -10,8 +10,10 @@ max_hp = 10
 hp = 10
 weapon = "dagger"
 weapon_damage = 2
+extra_damage = 0
 armor = "Nothing"
 armor_defense = 0
+extra_defense = 0
 extra_slot = "Nothing"
 board_dict = {}
 
@@ -108,6 +110,123 @@ def gen():
         else:
             place_object("C")
 
+def equip():
+    print("Do you want to equip this item? (y/n)")
+    choice = input()
+    return choice
+
+
+def attack(dx, dy):
+    k = id.index("x")
+    target_x = x[k] + dx
+    target_y = y[k] + dy
+    for n in range(len(id)):
+        if x[n] == target_x and y[n] == target_y:
+            if id[n] == "C":
+                action_taken = True
+                print("You open the chest and find treasure!")
+                id[n] = "□"
+                if lvl == 1:
+                    treasure_roll = roll(4)
+                    if treasure_roll == 1:
+                        print("You found a dagger! (Damage 1d3)")
+                        if equip() == "y":
+                            weapon = "dagger"
+                            weapon_damage = 3
+                    elif treasure_roll == 2:
+                        print("You found a Leather Armor! (Defense +1)")
+                        if equip == "y":
+                            armor = "Leather Armor"
+                            armor_defense = 1
+                    elif treasure_roll == 3:
+                        print("You found a attack ring! (Damage +1)")
+                        if equip() == "y":
+                            extra_slot = "attack ring 1"
+                            extra_damage = 1
+                            extra_defense = 0
+                    elif treasure_roll == 4:
+                        print ("You found a a defense amulet! (Defense +1)")
+                        if equip() == "y":
+                            extra_slot = "defense amulet 1"
+                            extra_defense = 1
+                            extra_damage = 0
+                if lvl == 2:
+                    treasure_roll = roll(6)
+                    if treasure_roll == 1:
+                        print("You found a short sword! (Damage 1d6)")
+                        if equip() == "y":
+                            weapon = "short sword"
+                            weapon_damage = 6
+                    elif treasure_roll == 2:
+                        print("You found a Chainmail Armor! (Defense +2)")
+                        if equip() == "y":
+                            armor = "Chainmail Armor"
+                            armor_defense = 2
+                    elif treasure_roll == 3:
+                        print("You found a swift dagger! (Damage 1d3)")
+                        if equip() == "y":
+                            weapon = "swift dagger"
+                            weapon_damage = 3
+                    elif treasure_roll == 4:
+                        print("You found a attack ring! (Damage +1)")
+                        if equip() == "y":
+                            extra_slot = "attack ring 1"
+                            extra_damage = 1
+                            extra_defense = 0
+                    elif treasure_roll == 5:
+                        print ("You found a a defense amulet! (Defense +1)")
+                        if equip() == "y":
+                            extra_slot = "defense amulet 1"
+                            extra_defense = 1
+                            extra_damage = 0
+                    elif treasure_roll == 6:
+                        print ("You found a health potion! (Restores hp to max when used)")
+                        if equip() == "y":
+                            extra_slot = "health potion"
+                            extra_damage = 0
+                            extra_defense = 0
+                if lvl >= 3:
+                    treasure_roll = roll(6)
+                    if treasure_roll == 1:
+                        print("You found a longsword! (Damage 1d8)")
+                        if equip() == "y":
+                            weapon = "longsword"
+                            weapon_damage = 8
+                    elif treasure_roll == 2:
+                        print("You found a Plate Armor! (Defense +3)")
+                        if equip() == "y":
+                            armor = "Plate Armor"
+                            armor_defense = 3
+                    elif treasure_roll == 3:
+                        print("You found a swift dagger! (Damage 1d4)")
+                        if equip() == "y":
+                            weapon = "swift dagger"
+                            weapon_damage = 4
+                    elif treasure_roll == 4:
+                        print("You found a attack ring! (Damage +2)")
+                        if equip() == "y":
+                            extra_slot = "attack ring 2"
+                            extra_damage = 2
+                            extra_defense = 0
+                    elif treasure_roll == 5:
+                        print ("You found a a defense amulet! (Defense +2)")
+                        if equip() == "y":
+                            extra_slot = "defense amulet 2"
+                            extra_defense = 2
+                            extra_damage = 0
+                    elif treasure_roll == 6:
+                        print ("You found a health potion! (Restores hp to max when used)")
+                        if equip() == "y":
+                            extra_slot = "health potion"
+                            extra_damage = 0
+                            extra_defense = 0
+            elif id[n] != "□":
+                action_taken = True
+                print(f"You attack the {id[n]}!")
+                # Here you would implement combat mechanics
+            else:
+                return
+
 x.append(0)
 y.append(0)
 id.append("x")
@@ -168,11 +287,22 @@ while True:
         print("S - slime (HP: 2, Damage: 1d3) slimes can't move")
         if lvl >= 2:
             print("level 2 monsters:")
-            print("L - Lizardman (HP: 10, Damage: 1d3)")
+            print("L - Lizardman (HP: 10, Damage: 1d3, defense: 1)")
             print("F - Freakish Abberation (HP: 6, Damage: 1d6)")
             if lvl >= 3:
                 print("level 3 monsters:")
-                print("O - Orc (HP: 16, Damage: 1d4)")
-                print("T - Troll (HP: 20, Damage: 1d8)")
-
+                print("O - Orc (HP: 10, Damage: 1d8, defense: 2)")
+                print("T - Troll (HP: 16, Damage: 1d6, defense: 3)")
+    elif action == "aa":
+        action_taken = True
+        attack(-1, 0)
+    elif action == "dd":
+        action_taken = True
+        attack(1, 0)
+    elif action == "ww":
+        action_taken = True
+        attack(0, -1)
+    elif action == "ss":
+        action_taken = True
+        attack(0, 1)
 # print ("GAME OVER")
